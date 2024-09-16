ecbfad73e4a1cc97a4bec3109ff274eb6a09728e
Cronos477 <felipe.chess47@gmail.com>
2024-09-16 16:24:57 -0300

    Adição de sistema de pesquisa e novas imagens

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListarFotografias(admin.ModelAdmin):
+    list_display=("id", "nome", "categoria", "data_fotografia", "legenda", "publicar")
+    list_display_links = ("id", "nome", "categoria", "data_fotografia")
+    search_fields=("nome",)
+    list_filter=("categoria",)
+    list_editable = ("publicar",)
+    list_per_page=10
+
+admin.site.register(Fotografia, ListarFotografias)
