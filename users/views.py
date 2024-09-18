61ad078947258b8df2a70a80c3c83e7249cd2fe0
Cronos477 <felipe.chess47@gmail.com>
2024-09-18 16:59:36 -0300

    Adição página de login

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from users.forms import LoginForms
+
+def login(request):
+    form = LoginForms()
+    return render(request, "users/login.html", {"form": form})
+
+def cadastro(request):
+    return render(request, "users/cadastro.html")
